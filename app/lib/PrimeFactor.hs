6a7ff5685bb43d2a1585814d4b1df184b7834d7c
jueve <o8g7no9q@gmail.com>
2020-05-10 01:29:49 +0900

    `search`関数を変更

--- app/lib/PrimeFactor.hs
+++ app/lib/PrimeFactor.hs
@@ -7,15 +7,13 @@ primeFactor m n = loop n 2 m
         loop :: Int -> Int -> MP.Map Int Int ->  MP.Map Int Int
         loop n i m
             | n `rem` i == 0 = inner n i m
-            | i * i > n = if n /= 1 then search n m else m 
+            | i * i > n = if n /= 1 then add n m else m 
             | otherwise = loop n (i+1) m
 
         inner :: Int -> Int -> MP.Map Int Int -> MP.Map Int Int
         inner n i m
-            | n `rem` i == 0 = let m' = search i m in inner (n `quot` i) i m'
+            | n `rem` i == 0 = let m' = add i m in inner (n `quot` i) i m'
             | otherwise = loop n (i+1) m
-        
-        search :: Int -> MP.Map Int Int -> MP.Map Int Int 
-        search k m = case MP.lookup k m of
-            Just _ -> MP.adjust (+1) k m
-            Nothing -> MP.insert k 1 m
+
+        add :: Int -> MP.Map Int Int -> MP.Map Int Int 
+        add k m = MP.insertWith (+) k 1 m       
