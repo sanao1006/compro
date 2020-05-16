aa0e7a49d301834bac289ba9fcd0286c60b3f780
jueve <o8g7no9q@gmail.com>
2020-05-16 12:34:15 +0900

    ダイクストラ法を簡略化

--- app/lib/Dijkstra.hs
+++ app/lib/Dijkstra.hs
@@ -15,43 +15,42 @@ data Direction = Directed | Undirected
 buildGraph :: Direction -> Int -> V.Vector (Int, (Int, Int)) -> Graph
 buildGraph direction vertex pathInfo =
   case direction of 
-    Directed -> V.accumulate step (V.replicate vertex []) pathInfo
-    Undirected -> V.accumulate step (V.replicate vertex []) pathInfo'
+    Directed   -> V.accumulate f (V.replicate vertex []) pathInfo
+    Undirected -> V.accumulate f (V.replicate vertex []) pathInfo'
   where
-    step xs v = v:xs
-    pathInfo' = pathInfo V.++ (V.map (\(a,(b,c)) -> (b,(a,c))) pathInfo)
+    f = flip (:)
+    g (a,(b,c)) = (b,(a,c))
+    pathInfo' = pathInfo V.++ V.map g pathInfo
  
 dijkstra :: Int -> Int -> Graph -> IO Distance
 dijkstra vertex from graph = do
   distance <- VUM.replicate vertex (10^9) :: IO Distance
   VUM.write distance from 0 
-  trav graph distance (ST.singleton (0, from))
-  where
-    trav :: Graph -> Distance -> Queue -> IO Distance
-    trav graph distance queue
-      | ST.null queue = return distance
-      | otherwise = do
-          tmp <- VUM.read distance from
-          case compare acc tmp of
-            LT -> update from acc distance queue' >>= trav graph distance
-            EQ -> update from acc distance queue' >>= trav graph distance
-            GT -> trav graph distance queue'
-      where
-        ((acc, from), queue') = ST.deleteFindMin queue
- 
-    update :: Int -> Int -> Distance -> Queue -> IO Queue
-    update f a dist q = update' a dist (graph V.! f) q
+  go graph distance (ST.singleton (0, from))
 
-    update' :: Int -> Distance -> [(Int, Int)] -> Queue -> IO Queue
-    update' _ _ [] q = return q
-    update' a dist ((to, cost):xs) q = do
-      tmp <- VUM.read dist to
-      case compare a' tmp of
-        LT -> VUM.write dist to a' >> update' a dist xs q'
-        _ -> update' a dist xs q
+go :: Graph -> Distance -> Queue -> IO Distance
+go graph distance queue
+  | ST.null queue = return distance
+  | otherwise = do
+      tmp <- VUM.read distance from
+      case compare acc tmp of
+        LT -> updateQueue from acc vs distance queue' >>= go graph distance
+        EQ -> updateQueue from acc vs distance queue' >>= go graph distance
+        GT -> go graph distance queue'
+  where
+    ((acc, from), queue') = ST.deleteFindMin queue
+    vs = graph V.! from
+ 
+updateQueue :: Int -> Int -> [(Int, Int)] -> Distance -> Queue -> IO Queue
+updateQueue _ _ [] _ queue = return queue
+updateQueue from acc ((to, cost):xs) distance queue = do
+    tmp <- VUM.read distance to
+    case compare acc' tmp of
+        LT -> VUM.write distance to acc' >> updateQueue from acc xs distance queue'
+        _  -> updateQueue from acc xs distance queue
       where
-        a' = a + cost
-        q' = ST.insert (a', to) q
+        acc' = acc + cost
+        queue' = ST.insert (acc', to) queue
 
 convToPathInfo bs = (s-1, (t-1, d))
   where 
